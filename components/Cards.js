b9a002aad9cd58c5383dcc67812ff785707635a6
sofiakung5@gmail.com <sofiakung5@gmail.com>
2021-08-13 21:26:37 +0800

    added icons

--- components/Cards.js
+++ components/Cards.js
@@ -1,4 +1,10 @@
 import classes from './Cards.module.css'
+import {
+  ChatAltIcon,
+  TrashIcon,
+  PencilAltIcon,
+  HeartIcon,
+} from '@heroicons/react/outline'
 
 export default function Cards(props) {
   return (
@@ -6,6 +12,20 @@ export default function Cards(props) {
       <div className={classes.card}>
         <h4 className={classes.title}>{props.chapter}</h4>
         <p className={classes.para}>{props.highlight}</p>
+        <div className={classes.iconSection}>
+          <button className={classes.button}>
+            <ChatAltIcon className={classes.icon} />
+          </button>
+          <button className={classes.button}>
+            <PencilAltIcon className={classes.icon} />
+          </button>
+          <button className={classes.button}>
+            <HeartIcon className={classes.icon} />
+          </button>
+          <button className={classes.button}>
+            <TrashIcon className={classes.icon} />
+          </button>
+        </div>
       </div>
     </>
   )
