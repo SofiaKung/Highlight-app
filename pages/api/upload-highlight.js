ed031327dca6df8d8afc936dab8cf914411b89e6
sofiakung5@gmail.com <sofiakung5@gmail.com>
2021-08-14 15:18:43 +0800

    set up mongo db

---
+++ pages/api/upload-highlight.js
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// /api/upload-highlight
+import { MongoClient } from 'mongodb'
+
+async function handler(req, res) {
+  if (req.method == 'POST') {
+    const data = req.body
+
+    const client = await MongoClient.connect(
+      'mongodb+srv://sofiakung:highfive5@cluster0.12cdz.mongodb.net/highlights?retryWrites=true&w=majority',
+    )
+
+    const db = client.db()
+    const highlightCollection = db.collection('highlights')
+    const result = await meetupsCollection.insertOne(data)
+
+    console.log(result)
+  }
+}
+
+export default handler
