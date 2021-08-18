c63fd47079c5ffec9bcafbb76c69dfbecfc9253d
sofiakung5@gmail.com <sofiakung5@gmail.com>
2021-08-19 00:09:00 +0800

    comment out unused code

--- pages/api/read-highlights.js
+++ pages/api/read-highlights.js
@@ -1,19 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 // /api/read-highlights
 
+// import { connectToDatabase } from '../../utils/mongodb'
 
-import { connectToDatabase } from '../../utils/mongodb'
+// async function handler(req, res) {
+//   console.log('[readHighlight] triggered')
 
-async function handler(req, res) {
-  console.log('[readHighlight] triggered')
+//   const { db } = await connectToDatabase()
 
-  const { db } = await connectToDatabase()
+//   const highlightCollection = await db.collection('highlights')
 
-  const highlightCollection = await db.collection('highlights')
+//   const result = await highlightCollection.find({}).toArray()
 
-  const result = await highlightCollection.find({}).toArray()
+//   res.status(200).json(result)
+// }
 
-  res.status(200).json(result)
-}
-
-export default handler
+// export default handler
