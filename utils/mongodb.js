e3751e555f42c137c3090128a3e25b9dc0664dd8
sofiakung5@gmail.com <sofiakung5@gmail.com>
2021-08-26 12:16:47 +0800

    add env variables

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -1,20 +1,25 @@
 import { MongoClient } from 'mongodb'
 
-let uri = "mongodb+srv://highlightsUser:xR7AUiTvJDiO62We@cluster0.b9ii8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-let dbName = "highlights_app"
+let uri =
+  'mongodb+srv://' +
+  process.env.MONGO_DB_USER +
+  ':' +
+  process.env.MONGO_DB_PASS +
+  '@cluster0.12cdz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+let dbName = 'highlights_app'
 
 let cachedClient = null
 let cachedDb = null
 
 if (!uri) {
   throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
+    'Please define the MONGODB_URI environment variable inside .env.local',
   )
 }
 
 if (!dbName) {
   throw new Error(
-    'Please define the MONGODB_DB environment variable inside .env.local'
+    'Please define the MONGODB_DB environment variable inside .env.local',
   )
 }
 
